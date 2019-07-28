5375725a2f0a642abfe5c1681590c8f0f0632e65
MaxWS <maxdjob1@gmail.com>
2019-07-28 15:14:31 +1000

    Red Wings website link, spell, debbuging style

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// const effect = function() {
+// 	// Selects 
+// 	let item = document.getElementsByTagName("SPAN");
+// 	console.log(item);
+// 	for(let i=0; i<item.length; i++) {
+// 		item[i].classList.add("open");
+// 	}
+// }
+// window.addEventListener("load", () => {effect()}, );
